33461fbdc591612eee3edadac4f8ef20935f6eca
GawarWawar <gawarwawar@gmail.com>
2025-12-20 13:47:05 +0200

    feat: plans for data structure in models

--- Items_DB/models.py
+++ Items_DB/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Item (models.Model):
+    name = models.CharField(verbose_name="Item Name", max_length=100, primary_key=True)
+    # type = #solid/liquid
+    # max_stack_size = 
+    # raw = #True/False
+    # researches = 
+     
+class Receipe (models.Model):
+    ...
+    # name = models.CharField(verbose_name="Receipe Name", max_length=100, primary_key=True)
+    items = models.ManyToManyField(Item, through="Items_In_Recipe")
+    # time_to_produce = 
+    # output_quantity = 
+    # description =     
+    
+class Items_In_Recipe (models.Model):
+    ...
+    # receipe =
+    # item_name = 
+    # item_quantity =
+    
+class Building (models.Model):
+    ... 
+    # name =
+    # item = 
+    recipes = models.ManyToManyField(Receipe)
+    # speed =
+    # power_source = #electricity/burner
+    # power_consumption = #in watts
+    # drain = #can be NONE
+    # n_of_module_slots = #0/1/2/3/4
+    # productivity = #upps with productivity module, base = 1
