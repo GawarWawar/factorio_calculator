9f64bbd3ade8c7e0d0010ead1a703b69d746d5e8
GawarWawar <gawarwawar@gmail.com>
2025-12-21 18:19:18 +0200

    feat: adds column types for the Building entities

--- Items_DB/models.py
+++ Items_DB/models.py
@@ -25,17 +25,21 @@ class Items_In_Recipe (models.Model):
     item_quantity = models.FloatField(verbose_name="Quantity of Items Needed for Recipe")
     
 class Building (models.Model):
-    ... 
-    # name =
-    # item = 
+    ENERGY_SOURCES = {
+        "elc" : "electricity",
+        "brn" : "burner",
+    }
+    
+    name = models.CharField(verbose_name="Building Name", max_length=100, unique=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     recipes = models.ManyToManyField(Recipe)
-    # speed =
-    # energy_source = #electricity/burner
-    # power_consumption = #in watts
-    # drain = #can be NONE
-    # pollution = 
-    # n_of_module_slots = #0/1/2/3/4
-    # productivity = #upps with productivity module, base = 1
+    speed = models.FloatField(verbose_name="Building Production Speed")
+    energy_source = models.CharField(verbose_name="Building Energy Source", max_length=3, choices=ENERGY_SOURCES) #electricity/burner
+    energy_consumption = models.FloatField(verbose_name="Building Energy Consumption") #in Kwatts
+    energy_drain = models.FloatField(verbose_name="Building Energy Drain", blank=True) #can be NONE
+    pollution = models.FloatField(verbose_name="Building Pollution", blank=True) #can be NONE
+    n_of_module_slots = models.IntegerField(verbose_name="Building Module Slots") #0/1/2/3/4
+    productivity = models.FloatField(verbose_name="Building Productivity Rate", default=1) #upps with productivity module, base = 1
 
     
 class Module(models.Model):
